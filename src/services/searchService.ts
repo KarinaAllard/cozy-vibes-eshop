c2cf329a303ea01ac38253a73824f5cef443aae6
Karina Allard <karina.allard.92@gmail.com>
2025-04-09 10:47:31 +0200

    Fix the url to match the backend.

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -4,7 +4,7 @@ import { API_URL, handleRequest } from "./baseService";
 
 export const searchService = async (searchInput: string): Promise<ISearchItem[]> => {
     return await handleRequest(
-        axios.get(`${API_URL}/search`, {
+        axios.get(`${API_URL}/search/search`, {
             params: { q: searchInput },
         })
     )
