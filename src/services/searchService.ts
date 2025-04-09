29d605b6ac18e9b67fc063e3b1ba8813720e50e1
Karina Allard <karina.allard.92@gmail.com>
2025-04-09 10:41:31 +0200

    Fix typo in query

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -5,7 +5,7 @@ import { API_URL, handleRequest } from "./baseService";
 export const searchService = async (searchInput: string): Promise<ISearchItem[]> => {
     return await handleRequest(
         axios.get(`${API_URL}/search`, {
-            params: { s: searchInput },
+            params: { q: searchInput },
         })
     )
 }
