53abce905db35034e25ed1e27d9ec4514c32d42d
Karina Allard <karina.allard.92@gmail.com>
2025-04-09 12:05:52 +0200

    Add a loading check to avoid confusion when the API is not loading fast enough to display results. Add truncate to the title since they are very long.

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -7,6 +7,10 @@ export const Searchbar = () => {
     const [searchText, setSearchText] = useState<string>("");
     const { searchResults, error, isLoading } = useSearch(searchText);
 
+    const truncateTitle = (title: string, length: number) => {
+        return title.length > length ? title.substring(0, length) + '...' : title;
+    }
+
     useEffect(() => {
         if (searchText.length < 3) {
             return;
@@ -24,20 +28,29 @@ export const Searchbar = () => {
                 className="border border-[var(--warm-light-gray)] bg-[var(--product-card-bg)] px-2 py-0.5 rounded-sm focus:outline-0"
             />
             {searchText.length >= 3 && (
-                <div className="absolute top-full left-0 w-full bg-[var(--soft-ivory)] border border-[var(--warm-light-gray)]">
+                <div className="absolute max-h-[500px] overflow-y-scroll md:max-h-[650px] top-full left-0 w-full bg-[var(--soft-ivory)] border border-[var(--warm-light-gray)]">
                 {error ? (
                     <p className="p-2 text-[var(--burnt-red)]">Error fetching search results.</p>
                 ) : (
                     <div className="flex flex-col">
-                        {searchResults.length > 0 ? (
+                        <Link to={"/search"} className="bg-[var(--dusty-rose)]"><p className="text-2xl text-[var(--soft-ivory)] hover:text-[var(--product-card-bg)]">View all</p></Link>
+                        
+                        {isLoading ? (
+                            <p className="p-2 text-[var(--muted-gold)]">Searching...</p>
+                        ) : searchResults.length > 0 ? (
                             searchResults.slice(0, 5).map((item) => (
                             <div key={item.link} className="p-2 border-b border-[var(--warm-light-gray)]">
-                                <Link to={item.link} target="_blank" rel="noopener noreferrer" className="underline text-[var(--muted-gold)] hover:text-[var(--muted-gold-hv)]"><p>{item.title}</p></Link>
+                                {item.pagemap.cse_thumbnail && (
+                                    <img src= {item.pagemap.cse_thumbnail[0].src} alt={item.title}
+                                    className="w-[200px]" />
+                                )}
+                                <Link to={item.link} target="_blank" rel="noopener noreferrer" className="underline text-[var(--muted-gold)] hover:text-[var(--muted-gold-hv)]"><p className="underline text-[var(--muted-gold)] hover:text-[var(--muted-gold-hv)]">{truncateTitle(item.title, 40)}</p></Link>
                             </div>
                             ))
                         ) : (
-                            <p>No results found</p>
+                            <p className="p-2 text-[var(--warm-light-gray)]">No results found</p>
                          )}
+                        
                     </div>
                 )}
             </div>
