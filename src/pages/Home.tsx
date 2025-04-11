d18db4a98361e7789115cdb994793feffd92a671
Karina Allard <karina.allard.92@gmail.com>
2025-04-11 17:46:31 +0200

    Add more styling and disclaimer to the home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,46 @@
 // import "../styles/home.css"
 
+import { Link } from "react-router";
 import { Button } from "../components/Button";
 
 export const Home = () => {
 	return (
-		<div className="home-wrapper flex items-center flex-col">
-			<div className="p-8 flex items-center gap-3">
-  				<h1 className="text-[var(--moss-green)]">Welcome to </h1><img src="/images/logo.png" alt="Cozy Vibes" className=" w-24 " />
+		<div className="flex flex-col items-center mb-20">
+			<h1 className="bg-[var(--dusty-rose)] text-[var(--soft-ivory)] w-full mb-10 ">
+				Cozy, Calming Decor
+			</h1>
+
+			<div className="flex flex-col items-center">
+				<h2 className="text-[var(--moss-green)]">Welcome to </h2>
+				<img src="/images/logo.png" alt="Cozy Vibes" className="w-28" />
+				<div className="w-full max-w-7xl px-4">
+					<div className="p-8">
+						<p className="text-[var(--soft-charcoal)]">
+							Discover our selection of cozy home decor items.
+						</p>
+						<Button variant="primary" className="mt-4"><Link to={"/products"}>Shop Now</Link></Button>
+					</div>
+					<section className="p-8">
+						<h3>Our Purpose</h3>
+						<p className="mt-2">
+							The purpose of this little eshop is not to sell anything. The
+							products displayed on this website are purely for educational
+							purposes, as this is a school project that I'm making for my class
+							at Medieinstitutet. I'm learning how to build full e-commerce
+							functionality using modern web technologies, and this project is a
+							way for me to put all that into practice. So while you can browse,
+							search, and even add things to your cart, none of it is actually
+							for sale - just part of the learning process!
+						</p>
+						<br />
+						<p>
+							All product images and styles are borrowed from Lagerhaus and are
+							used here solely for educational purposes. This site is not
+							affiliated with or endorsed by Lagerhaus in any way.
+						</p>
+					</section>
+				</div>
 			</div>
-
-			<h2 className="text-[var(--dusty-rose)]">Cozy, Calming Decor</h2>
-			<p className="text-[var(--soft-charcoal)]">Discover our selection of cozy home decor items.</p>
-
-			<Button variant="primary" className="">
-  				Shop Now
-			</Button>
 		</div>
 	);
 };
